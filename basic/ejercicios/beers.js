f7b942dd5ca368665fee1e317ca7c1844fbb171d
Ignacio Giss <rgiss@mulesoft.com>
2021-09-05 22:39:06 -0300

    beers

--- basic/ejercicios/beers.js
+++ basic/ejercicios/beers.js
@@ -19,3 +19,63 @@ const beers = [
     { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
     { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
   ];
+
+
+/**
+ * 
+ * @param {*} beer 
+ * @returns {int}
+ */
+  function obtenerPrecio(beer) {
+    if (beer.name == "Purple Iris")
+      return 320;
+
+    if (beer.abv < 5.0)
+      return 300;
+    
+    return 350;
+  }
+
+  /**
+   * 
+   * @param {Array} beersArray 
+   * @returns Array con los precios
+   */
+  function obtenerPrecios(beersArray) {
+    return beersArray.map(b => ({
+      ...b,
+      price: obtenerPrecio(b)
+    }));
+  }
+
+  /**
+   * 
+   * @param {Array} beersArray 
+   * @returns Array con los precios
+   */
+  function insertarFileName(beersArray) {
+    return beersArray.map(b => ({
+      ...b,
+      file_name: b.label.replace(/^.*[\\\/]/, '')
+    }));
+  }
+
+  /**
+   * 
+   * @param {Array} beersArray 
+   * @returns Array con los precios
+   */
+   function ordenarPorTipo(beersArray) {
+    return beersArray.sort((a, b) => (a.type > b.type) ? 1 : -1)
+  }
+
+console.log("1.-");
+let beersPriced = obtenerPrecios(beers);
+console.log(JSON.stringify(beersPriced));
+
+console.log("2.-");
+let beersFileName = insertarFileName(beersPriced)
+console.log(JSON.stringify(beersFileName));
+
+console.log("3.-");
+console.log(JSON.stringify(ordenarPorTipo(beersFileName)));
