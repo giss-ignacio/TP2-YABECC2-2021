e3d1e425f81be13627ef712410d74ecf37d86c5e
Ignacio Giss <rgiss@mulesoft.com>
2021-09-07 20:51:21 -0300

    inventors

--- filesystem/inventor.js
+++ filesystem/inventor.js
@@ -16,18 +16,42 @@ function getInventor(id){
 
 // Alta
 function pushInventor(inventor){
-
+    let inventors = getInventors();
+    inventors.push(inventor);
+    fs.writeFileSync(PATH, JSON.stringify({"inventors": inventors}, null, 4), 'utf-8');
 }
 
 // Modificacion 
 function updatIventor(id, inventor){
+    let inventors = getInventors();
+    
+    
+    let index = inventors.findIndex((inv => inv._id == id));
 
+    if (index != -1) {
+        inventors[index]._id = inventor._id;
+        inventors[index].first = inventor.first;
+        inventors[index].last = inventor.last;
+        inventors[index].year = inventor.year;
+    
+        fs.writeFileSync(PATH, JSON.stringify({"inventors": inventors}, null, 4), 'utf-8');
+    } else {
+        console.log("ERROR: id " + id + " not found.");
+    }
 }
 
 // Eliminacion
 function deleteInventor(id){
+    let inventors = getInventors();
 
+    let index = inventors.findIndex((inv => inv._id == id));
+    if (index != -1) {        
+        inventors.splice(index, 1);
+
+        fs.writeFileSync(PATH, JSON.stringify({"inventors": inventors}, null, 4), 'utf-8');
+    } else {
+        console.log("ERROR: id " + id + " not found.");
+    }
 }
 
 module.exports = {getInventor, getInventors, pushInventor, deleteInventor, updatIventor};
-
